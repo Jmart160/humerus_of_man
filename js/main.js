4835916743335286756d86cf6c76af267b2b2d93
jmart160 <jmart160@ucsc.edu>
2018-08-26 19:05:18 -0700

    working main code placed in master again

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// define globals
+var game;
+var mainTitle;
+var instructions;
+var item=null;
+var interactable=null;
+var type=null;
+var popUp = false;
+var box=false;
+var lineCount=null;
+var count=null;
+var storedAction=null;
+var storedSubject=null;
+var playerUp=false;
+var interactableUp=false;
+
+//waiting for browser to load before creating Phaser game
+window.onload = function(){
+    // keep following line commented
+    // localStorage.clear();
+
+    // create ratio
+    game = new Phaser.Game(1400, 800, Phaser.AUTO, '');
+
+    //states here
+    game.state.add('Load', Load);
+    //game.state.add('MainMenu', MainMenu );
+    game.state.add('townCenter', townCenter);
+    game.state.start('Load');
+}
